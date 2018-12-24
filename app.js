bf85d5a584c040ec262618e317245ea4327f1ede
suhailakhtar039 <suhailakhtar039@gmail.com>
2018-12-24 19:32:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express=require("express");
+var app=express();
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+	res.render("home");
+});
+app.listen(3000,function(){
+	console.log("server has started");
+});
